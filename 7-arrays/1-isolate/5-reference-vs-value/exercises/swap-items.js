d5b01a6a72b5e81622eed6c31d2895710f4cd79b
artur <grit4in0225@icloud.com>
2025-03-14 07:56:42 +0100

    finish arrays-isolate

--- 7-arrays/1-isolate/5-reference-vs-value/exercises/swap-items.js
+++ 7-arrays/1-isolate/5-reference-vs-value/exercises/swap-items.js
@@ -12,6 +12,12 @@ let temp;
 
 // -- swap values (write this code) --
 
+temp = arr1[2]; // 'b'
+arr1[2] = arr2[0]; // [1, 2, 3, 4]
+arr2[0] = temp; // ['b', 'a', 'c']
+arr2[0] = arr2[1]; // ['a', 'a', 'c']
+arr2[1] = temp; // ['a', 'b', 'c']
+
 // -- assert values (this is correct) --
 console.assert(deepCompare(arr1, [1, 2, 3, 4]), 'array 1');
 console.assert(deepCompare(arr2, ['a', 'b', 'c']), 'array 2');
